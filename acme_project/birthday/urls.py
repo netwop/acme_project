1a0704252367de26765269ddf632233b10d437fa
Alexandra Shelukhina <sasha50395@yandex.ru>
2025-11-09 17:55:41 +0300

    тренировка форм, картинки

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'birthday'
 
-urlpatterns = [
-    path('', views.birthday, name='create'),
-    path('list/', views.birthday_list, name='list')
+urlpatterns = [ path('', views.birthday, name='create'),
+    path('list/', views.birthday_list, name='list'),
     path('<int:pk>/edit/', views.birthday, name='edit'),
-    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
-]
+    path('<int:pk>/delete/', views.delete_birthday, name='delete')]
+ 
+    
